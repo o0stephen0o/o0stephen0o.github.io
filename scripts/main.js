371eafb74c7ebfa5ea01b8eac59bb516e3a6146a
o0stephen0o <58679743+o0stephen0o@users.noreply.github.com>
2022-02-25 14:11:24 +0800

    19

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function() {
       myImage.setAttribute('src', 'images/moon.jpg');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = '歡迎你,' + myName;
+  }
+
+if(!localStorage.getItem('name')) {
+setUserName();
+} else {
+let storedName = localStorage.getItem('name');
+myHeading.textContent = 'Mozilla 酷毙了,' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+ }
