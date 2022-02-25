3346e1820ef6e27f85e94be2cdab8c9fcc06a85e
o0stephen0o <58679743+o0stephen0o@users.noreply.github.com>
2022-02-25 13:45:28 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/moon.jpg') {
-      myImage.setAttribute('src', 'images/moon.jpg');
-    } else {
       myImage.setAttribute('src', 'images/watch.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/moon.jpg');
     }
 }
